99eee81c18a002b000f2ac0826ee1322bf52f5ac
k0varex <celrunya@gmail.com>
2025-08-10 11:49:05 +0200

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Nodig om tekst naar het scherm te sturen
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // Print tekst naar het scherm
+    return 0; // Eindigt het programma
+}
---
+++ name_and_age.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Danny, 31" << std::endl;
+    return 0;
+}
