779d5e6bb50a8f47ad32eb07ac4c22caa9338235
Danny Tan <celrunya@gmail.com>
2025-08-25 18:11:53 +0200

    Add files via upload

--- even_oneven.cpp
+++ even_oneven.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <limits>
 
 int main() {
     int cijfer;
-    std::cout <<"Voer een cijfer in!" << std::endl;
-
-    std::cin >> cijfer;
-
+    while (true) {
+        std::cout <<"Voer een cijfer in!" << std::endl;
+        std::cin >> cijfer;
+        if (std::cin.fail()) {
+            std::cout << "Voer alleen cijfers in" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            break;
+        }
+    }
+    
     if (cijfer % 2 == 0) {
         std::cout <<"De cijfer is een even getal!" << std::endl;
     } else {
